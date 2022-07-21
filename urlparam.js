6e5f9a17ea636dd4bb2ec0c1acfc70abe009a996
GrowthGorilla <109748017+GrowthGorilla@users.noreply.github.com>
2022-07-21 18:00:09 +0300

    Create urlparam.js

---
+++ urlparam.js
@@ -0,0 +1,76 @@
+(function () {
+  //Getting the url of the page
+  const url_string = window.location.href;
+  //Temp URL
+  var url = new URL(url_string);
+  //Change the words myparam to the name of your parameter.
+  const promo = url.searchParams.get("promo");
+
+  //Multiple options case:
+  switch (promo) {
+    case "enmetaapple":
+      document.getElementById("lp-pom-button-15").href =
+        "https://heygotradeid.app.link/en-meta-apple";
+      break;
+    case "idmetaapple":
+      document.getElementById("lp-pom-button-15").href =
+        "https://heygotradeid.app.link/ID-Meta-Apple";
+      break;
+      case "entwitterapple":
+      document.getElementById("lp-pom-button-15").href =
+        "https://heygotradeid.app.link/EN-Twitter-Apple";
+      break;
+      case "idtwitterapple":
+      document.getElementById("lp-pom-button-15").href =
+        "https://heygotradeid.app.link/ID-Twitter-Apple";
+      break;
+      case "ensearchapple":
+      document.getElementById("lp-pom-button-15").href =
+        "https://heygotradeid.app.link/EN-Search-Apple";
+      break;
+      case "idsearchapple":
+      document.getElementById("lp-pom-button-15").href =
+        "https://heygotradeid.app.link/ID-Search-Apple";
+      break;
+      case "entiktokapple":
+      document.getElementById("lp-pom-button-15").href =
+        "https://heygotradeid.app.link/EN-TikTok-Apple";
+      break;
+      case "idtiktokapple":
+      document.getElementById("lp-pom-button-15").href =
+        "https://heygotradeid.app.link/ID-TikTok-Apple";
+      break;
+      case "enmetatesla":
+      document.getElementById("lp-pom-button-15").href =
+        "https://heygotradeid.app.link/EN-Meta-Tesla";
+      break;
+      case "idmetatesla":
+      document.getElementById("lp-pom-button-15").href =
+        "https://heygotradeid.app.link/ID-Meta-Tesla";
+      break;
+      case "entwittertesla":
+      document.getElementById("lp-pom-button-15").href =
+        "https://heygotradeid.app.link/EN-Twitter-Tesla";
+      break;
+      case "idtwittertesla":
+      document.getElementById("lp-pom-button-15").href =
+        "https://heygotradeid.app.link/ID-Twitter-Tesla";
+      break;
+      case "ensearchtesla":
+      document.getElementById("lp-pom-button-15").href =
+        "https://heygotradeid.app.link/EN-Search-Tesla";
+      break;
+      case "idsearchtesla":
+      document.getElementById("lp-pom-button-15").href =
+        "https://heygotradeid.app.link/ID-Search-Tesla";
+      break;
+      case "entiktoktesla":
+      document.getElementById("lp-pom-button-15").href =
+        "https://heygotradeid.app.link/EN-TikTok-Tesla";
+      break;
+      case "idtiktoktesla":
+      document.getElementById("lp-pom-button-15").href =
+        "https://heygotradeid.app.link/ID-TikTok-Tesla";
+      break;
+  }
+})();
